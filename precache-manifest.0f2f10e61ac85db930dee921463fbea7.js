813022927d7c931332f930694d650697c2eec4bf
madhu <madhu@micropyramid.com>
2020-05-09 13:31:59 +0530

    Updates

---
+++ precache-manifest.0f2f10e61ac85db930dee921463fbea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47cc17964ec7c58f657ebfbdbc23c61",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fb8b2f312203678953b7",
+    "url": "/monsters-rolodex/static/css/main.7896d7d1.chunk.css"
+  },
+  {
+    "revision": "f05237ce14ba31f66c50",
+    "url": "/monsters-rolodex/static/js/2.66d1f31a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.66d1f31a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8b2f312203678953b7",
+    "url": "/monsters-rolodex/static/js/main.00540364.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
