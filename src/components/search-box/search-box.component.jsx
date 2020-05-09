7c3679b66547daf33edfca0b83a6f42902e71daa
madhu <madhu@micropyramid.com>
2020-05-09 13:16:30 +0530

    adding files to github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className='search'
+    type='search'
+    placeholder={placeholder}
+    onChange={handleChange}
+  //   e => {
+  //     this.setState({ searchField: e.target.value }
+  //         // , () =>
+  //         // console.log(this.state)
+  //     );
+  //     // console.log(this.state.string);
+  // }}
+  />
+)
